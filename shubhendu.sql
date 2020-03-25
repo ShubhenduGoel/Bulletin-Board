c6f0a4725137f1e6ce07d2da36ce8e637a8c81c8
ShubhenduGoel <smartyshubhendu@gmail.com>
2020-03-25 13:52:52 +0530

    Add files via upload

--- shubhendu.sql
+++ shubhendu.sql
@@ -28,7 +28,7 @@ CREATE TABLE `boards` (
   `name` varchar(30) NOT NULL,
   `archive` int(11) DEFAULT '0',
   PRIMARY KEY (`id`,`owner`,`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `boards` (
 
 LOCK TABLES `boards` WRITE;
 /*!40000 ALTER TABLE `boards` DISABLE KEYS */;
-INSERT INTO `boards` VALUES (4,'shubhendu','Micro Processor',0),(7,'abc','abc',1),(9,'101703633','Graph Theory',0),(10,'101703633','Hello World',0),(11,'101703633','shubhendugoel',1),(12,'101703633','shubhendu',0),(13,'101703633','stuti',1),(14,'101703633','structures',1),(17,'101703633','Data Structures',0);
+INSERT INTO `boards` VALUES (4,'shubhendu','Micro Processor',0),(7,'abc','abc',1),(18,'101703633','Data Structures',0),(22,'shubhendu','Data Structures',0),(23,'abc','Data Structures',0);
 /*!40000 ALTER TABLE `boards` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,7 +52,8 @@ CREATE TABLE `cards` (
   `name` varchar(20) DEFAULT NULL,
   `list` varchar(20) DEFAULT NULL,
   `board` varchar(20) DEFAULT NULL,
-  `archive` int(11) DEFAULT '0'
+  `archive` int(11) DEFAULT '0',
+  `imp` varchar(20) DEFAULT 'medium'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -62,7 +63,7 @@ CREATE TABLE `cards` (
 
 LOCK TABLES `cards` WRITE;
 /*!40000 ALTER TABLE `cards` DISABLE KEYS */;
-INSERT INTO `cards` VALUES ('Stack','Hello','Graph Theory',0),('Data Structures','Data Structures','Hello World',0),('heello word','Data Structures','Hello World',0);
+INSERT INTO `cards` VALUES ('Data Structures','To Do','Data Structures',0,'medium'),('Hello','To Do','Data Structures',0,'Low'),('aaa','To Do','Data Structures',0,'High');
 /*!40000 ALTER TABLE `cards` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,8 +87,52 @@ CREATE TABLE `lists` (
 
 LOCK TABLES `lists` WRITE;
 /*!40000 ALTER TABLE `lists` DISABLE KEYS */;
-INSERT INTO `lists` VALUES ('Data Structures','Hello World',0),('Hello','Graph Theory',0);
+INSERT INTO `lists` VALUES ('To Do','Data Structures',0),('In Progress','Data Structures',0);
 /*!40000 ALTER TABLE `lists` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `temp`
+--
+
+DROP TABLE IF EXISTS `temp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `temp` (
+  `username` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `temp`
+--
+
+LOCK TABLES `temp` WRITE;
+/*!40000 ALTER TABLE `temp` DISABLE KEYS */;
+INSERT INTO `temp` VALUES ('101703633'),('shubhendu'),('abc');
+/*!40000 ALTER TABLE `temp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `temp1`
+--
+
+DROP TABLE IF EXISTS `temp1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `temp1` (
+  `username` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `temp1`
+--
+
+LOCK TABLES `temp1` WRITE;
+/*!40000 ALTER TABLE `temp1` DISABLE KEYS */;
+INSERT INTO `temp1` VALUES ('stuti');
+/*!40000 ALTER TABLE `temp1` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -99,7 +144,8 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `username` varchar(25) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL
+  `password` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -109,7 +155,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('shubhendu','81dc9bdb52d04dc20036dbd8313ed055'),('stuti','81dc9bdb52d04dc20036dbd8313ed055'),('101703633','81dc9bdb52d04dc20036dbd8313ed055'),('abc','900150983cd24fb0d6963f7d28e17f72');
+INSERT INTO `users` VALUES ('shubhendu','81dc9bdb52d04dc20036dbd8313ed055',NULL),('stuti','81dc9bdb52d04dc20036dbd8313ed055',NULL),('101703633','81dc9bdb52d04dc20036dbd8313ed055',NULL),('abc','900150983cd24fb0d6963f7d28e17f72',NULL),('shubhendugoel','a3f83e59e8141cc43cdd883d434e132f','smartyshubhendu@gmail.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -122,4 +168,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-23  2:00:38
+-- Dump completed on 2020-03-25 13:40:49
